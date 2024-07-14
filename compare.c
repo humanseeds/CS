099463629e5130c28409f9d9c2051be95ae88ea8
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-14 04:49:10 +0000

    Sat, Jul 13, 2024, 11:49 PM -05:00

--- compare.c
+++ compare.c
@@ -10,12 +10,12 @@ int main(void)
     {
         printf("x is less than y\n");
     }
-else if (x > y)
-{
-    printf("x is greater than y\n");
-}
-else if (x == y)
-{
-    printf("x is equal to y\n");
-}
+    else if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+    else if (x == y)
+    {
+        printf("x is equal to y\n");
+    }
 }
