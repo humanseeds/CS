bf8cd902077c5bb8c2c2c65d0a6a944cc8a4d8e2
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-14 18:37:23 +0000

    Sun, Jul 14, 2024, 1:37 PM -05:00

--- calculator.c
+++ calculator.c
@@ -1,15 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
-int add(void)
+
+int add(void);
+
 int main(void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
 
-    printf("%i\n", x + y);
+    int z = add();
+    printf("%i\n", z);
 }
 
 int add(void)
 {
-
+    return x + y;
 }
+
