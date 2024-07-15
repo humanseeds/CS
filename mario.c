2786df256dd835998beb6cfc15b5978c064e7f97
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-15 01:40:02 +0000

    Sun, Jul 14, 2024, 8:40 PM -05:00

--- mario.c
+++ mario.c
@@ -3,13 +3,14 @@
 
 int main(void)
 {
-    int n;
+    // Prompt user for positive integer
     do
     {
         n = get_int("Size: ");
     }
     while (n < 1);
 
+    //Print an n-by-n grid of blocks
     for (int i = 0; i < n; i++)
     {
        for (int j = 0; j < n; j++)
