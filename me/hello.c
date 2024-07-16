4f58de7179195c4f253032861fb356b6da8cecf6
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-16 04:13:24 +0000

    Mon, Jul 15, 2024, 11:13 PM -05:00

--- me/hello.c
+++ me/hello.c
@@ -1,10 +1,10 @@
-//Libraries 
+// Libraries
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    //Prompt user for their name
+    // Prompt user for their name
     string answer = get_string("What's your name?");
 
     // Print a greeting based on theirt name
