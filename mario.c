f6f5eba4a2e9ad6ed626092cf6272196c7d02e62
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-17 04:23:32 +0000

    Tue, Jul 16, 2024, 11:23 PM -05:00

--- mario.c
+++ mario.c
@@ -6,11 +6,13 @@ int main(void)
 {
    int n;
    do
+//prompt height of pyramid with a positive integer
    {
          n = get_int("Height: ");
    }
     while (n < 1);
 
+// p
     for (int i = 0; i < n; i++)
     {
        for (int j = 0; j < n;  j++)
