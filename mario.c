b503727cd3c1c478d82e3d28d48ba272e36d03f3
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-17 01:35:15 +0000

    Tue, Jul 16, 2024, 8:35 PM -05:00

--- mario.c
+++ mario.c
@@ -11,9 +11,9 @@ int main(void)
     }
     while (n < 1);
 
-    for (int i = 0; i + n; i++)
+    for (int i = 0; i < n; i++)
     {
-       for (int j = 0; j + n; j++)
+       for (int j = 0; j < n; j++)
         {
             printf("#");
         }
