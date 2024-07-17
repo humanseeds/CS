ec06b73e1ba7703883f2e2aaef382b683636c3e2
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-17 20:56:55 +0000

    Wed, Jul 17, 2024, 3:56 PM -05:00

--- practice_mario.c
+++ practice_mario.c
@@ -12,9 +12,12 @@ int main(void)
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j <= i; j++)
+        for (int j = 0; j < = -n - i; j++)
         {
-            printf("#");
+            printf(" ");
+            {
+                printf("#")
+            }
         }
         printf("\n");
     }
