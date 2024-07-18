c80159763896a34128191aad1b00a5f991155c97
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-18 01:54:45 +0000

    Wed, Jul 17, 2024, 8:54 PM -05:00

--- mario2.0.c
+++ mario2.0.c
@@ -13,6 +13,10 @@ int main(void)
     for (int i = 0; i < n; i++)
     {
       for  (int j = 0; j < n - i -1; j++)
-        printf(" ");
+      {
+          printf(" ");
+      }
+      
+        printf("#");
     }
 
