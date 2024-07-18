df6e6b34b72c7b2b7df3cef0d53a2e2ca7e9ed1c
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-18 01:53:32 +0000

    Wed, Jul 17, 2024, 8:53 PM -05:00

--- mario2.0.c
+++ mario2.0.c
@@ -3,7 +3,16 @@
 
 int main(void)
 {
+    int n
     do
     {
-        
+        n = get_int("Height: ")
     }
+     while (n < 1 || n > 8)
+
+    for (int i = 0; i < n; i++)
+    {
+      for  (int j = 0; j < n - i -1; j++)
+        printf(" ");
+    }
+
