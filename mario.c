8bc3ed2dd208e92d58f5114422621734ca322e39
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-18 01:09:10 +0000

    Wed, Jul 17, 2024, 8:09 PM -05:00

--- mario.c
+++ mario.c
@@ -12,16 +12,17 @@ int main(void)
    }
     while (n < 1);
 
-// print a pyramid based on height N. the rows and columns are equal in size to N. int I represents rows
-//
+// print a pyramid based on height N. the rows and columns are equal in size to N. int I
+// represents rows
      for (int i = 0; i < n; i++)
     {
   // Int J determines the columns. Its the same
        for (int j = 0; j < n; j++)
         {
-    // this IF condition controls the spacing and # placement. Ex. Height(N) = 9, and I(row 0) then 9-0+1=8. 8 columns are " " before the "#" is printed. As spaces decrease # increases
-
-            if (j < n- i - 1 )
+    // this IF condition controls the spacing and # placement. Ex. Height(N) = 9, and I   increases
+    // (row 0) then 9-0+1=8. 8 columns are" " before the "#" is printed. As spaces decrease
+    // #
+            if (j < n - i - 1)
            {
                 printf(" ");
             }
