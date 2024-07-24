8c986059dc0c7995d9c65568bc9c46e7226d15db
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-24 00:37:49 +0000

    Tue, Jul 23, 2024, 7:37 PM -05:00

--- cash/cash.c
+++ cash/cash.c
@@ -13,10 +13,19 @@ int main(void)
         cents = get_int("Change owed: ");
     }
         while (cents < 0);
+        int quaters = calculate_quaters(cents);
+        cents = cents - (quaters * 25);
 }
+
 int calculate_quarters (int cents)
 {
-
+    int quarters = 0;
+    while (cents >= 25)
+    {
+        quaters++;
+        cents = cents - 25;
+    }
+    return quaters;
 }
 int calculate_dimes (int dimes)
 {
