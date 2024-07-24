0db7db71212b45dfd8e8a394d0faa3937677e7f0
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-24 00:48:58 +0000

    Tue, Jul 23, 2024, 7:48 PM -05:00

--- cash/cash.c
+++ cash/cash.c
@@ -27,16 +27,37 @@ int calculate_quarters (int cents)
     }
     return quaters;
 }
+
 int calculate_dimes (int dimes)
 {
-
+    int dimes = 0;
+    while (cents <= 10)
+    {
+        dimes++;
+        cents = cents - 10;
+    }
+    return dimes;
 }
+
 int calculate_nickels (int nickels)
 {
-
+    int nickles = 1;
+    while (cents <= 5)
+    {
+        nickles++;
+        cents =  cents - 5;
+    }
+    return nickles;
 }
+
 int calculate_pennies (int pennies)
 {
-
+    int pennies = 0;
+    while (cents <= 0)
+    {
+        pennies ++;
+        cents = cents - 1;
+    }
+    return pennies;
 }
 
