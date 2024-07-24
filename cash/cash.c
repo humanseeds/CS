d89273fc40698c91b858df8c89d72700c1571650
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-24 00:04:20 +0000

    Tue, Jul 23, 2024, 7:04 PM -05:00

---
+++ cash/cash.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int cents;
+    do
+    {
+        cents = get_int("Change owed: ");
+    }
+        while (cents < 0);
+}
+int calculate_quarters
+int calculate_dimes
+int calculate_nickels
+int calculate_pennies
+
