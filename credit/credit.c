c8359ef0c09641bbeb260683a27113b70de195f2
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-27 19:13:34 +0000

    Sat, Jul 27, 2024, 2:13 PM -05:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,15 +4,14 @@
 int main(void)
 {
     long CreditCardNumber;
-        do
         {
         CreditCardNumber = get_long("Input Credit Card Number: ");
-        if (CreditCardNumber <= 0)
+        while (CreditCardNumber <= 0)
         {
             printf("Please Try Again \n");
-        }
-        while (CreditCardNumber <= 0 );
-        
+            CreditCardNumber <= 0 );
+
+
         }
     }
 
