724d6b32d059d8a3357b6bb0a38b1eaf7ad00a34
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-27 19:53:48 +0000

    Sat, Jul 27, 2024, 2:53 PM -05:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,6 +5,8 @@ int main(void)
 {
     long CreditCardNumber;
     CreditCardNumber = get_long("Please Input Credit Card Number: ");
-    while (CreditCardNumber <= 0)
+    while
+    {
+        (CreditCardNumber <= 0)
+    }
 }
-
