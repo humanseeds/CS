c893197fc3d3c0005cd91287d4dc6ce51a288284
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-28 01:21:11 +0000

    Sat, Jul 27, 2024, 8:21 PM -05:00

--- credit/credit.c
+++ credit/credit.c
@@ -29,10 +29,15 @@ while (WorkingCard < 0)
 {
         // int digit uses the modulo to remove the last number from the WorkingCard
         // after the digit is removed / 10 removes the last digit from the card entirely
+
     int digit = WorkingCard % 10;
     WorkingCard /= 10;
-        // modulo 2 determines if a card is even or odd by a result of 0 or 1 ( a remainder).
-        // by using the logic of a boolena experation a 0 or 1 signified true or false
+
+        // modulo 2 determines if a number is even or odd by a result of 0 (no remainder) or 1 (a remainder).
+        // in Boolean expressions every non zero value is true, zero value is false.
+        // If the modulo of digit is 0
+        // if the modulo of a digit is 1
+
     if (Position % 2 == 0)
     {
         checksum +== digit;
