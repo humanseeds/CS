d199d4a4b7c7eaeb52539c51515910660674ef19
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-28 05:29:28 +0000

    Sun, Jul 28, 2024, 12:29 AM -05:00

--- credit/credit.c
+++ credit/credit.c
@@ -9,21 +9,21 @@ int main(void)
     // until one is given.
 
     long Card_Number = get_long("Please Input Credit Card Number: ");
-    while (Card_Number <= 0);
+    while (Card_Number <= 0)
     {
     // this loop continues to politely ask user for a card number and set Long to card number
 
         printf("Please Try Again \n");
-        Card_Number = getlong("Please input Credit Card Number: ");
+        Card_Number = get_long("Please input Credit Card Number: ");
     }
 
     // initialize or declare the starting value of the two vaiables evenpositionsum and odd positionsum. Poisition to zero
     // Int workingCard is used to keep the original Card-Number intact to veryify card type later
 
     int EvenPositionSum = 0;
-    Int OddPositionSum = 0;
+    int OddPositionSum = 0;
     int Position = 0;
-    int WorkingCard = Card_Number
+    int WorkingCard = Card_Number;
 
         // Now we create a loop that does two functions. Run the loop until Card_Number = 0
 
@@ -62,7 +62,7 @@ while (WorkingCard > 0)
         {
             OddPositionSum += doubledDigit;
         }
-     WorkingCard /= 10;
+     Position++;
     }
 }
 }
