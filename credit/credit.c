f73d10303202fc4af65f8b503561f7f43b06ea2b
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-28 00:35:44 +0000

    Sat, Jul 27, 2024, 7:35 PM -05:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,17 +1,24 @@
+// Include libraries to pull functions from
+
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
+    // prompt the user for a Credit Card number. if card number is lesss than 0 user is asked again
+    // until one is given.
+
     long Card_Number = get_long("Please Input Credit Card Number: ");
     while (Card_Number <= 0);
     {
+    // this loop continues to politely ask user for a card number and set Long to card number
         printf("Please Try Again \n");
         Card_Number = getlong("Please input Credit Card Number: ");
     }
-    int checksum = 0
-    int Digit = 0
-    
+    // initialize 
+    int Checksum = 0;
+    int Digit = 0;
+
 
 
 }
