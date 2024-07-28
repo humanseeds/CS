12c97eee0c10c81581c1075642f1f95de2ee143e
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-28 00:56:38 +0000

    Sat, Jul 27, 2024, 7:56 PM -05:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,18 +12,23 @@ int main(void)
     while (Card_Number <= 0);
     {
     // this loop continues to politely ask user for a card number and set Long to card number
+
         printf("Please Try Again \n");
         Card_Number = getlong("Please input Credit Card Number: ");
     }
+
     // initialize or declare the starting value of the two vaiables Checksum and Poisition to zero
+    // Int workingCard is used to keep the original Card-Number intact to veryify card type later
+
     int Checksum = 0;
     int Position = 0;
+    int WorkingCard = Card_Number
 
 // Now we create a loop that does two functions. Run the loop until Card_Number = 0
-while (Card_Number < 0)
+while (WorkingCard < 0)
 {
-    int digit = Card_Number % 10;   //This modulo
-    Card_Number /= 10;
+    int digit = WorkingCard % 10;   // This modulo extracts the last digit from WorkingCard
+    WorkingCard /= 10;              // divide by 10 Removes the last Digit from WorkingCard
 }
 
 }
