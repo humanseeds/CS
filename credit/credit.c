ddb4e53ddf51851bd1958b0fc808af9ac86133a6
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-07-28 05:43:09 +0000

    Sun, Jul 28, 2024, 12:43 AM -05:00

--- credit/credit.c
+++ credit/credit.c
@@ -55,7 +55,7 @@ while (WorkingCard > 0)
         int DoubledDigit = digit * 2;
         if (DoubledDigit > 9 )
         {
-            OddPositionSum += (digit - 9)
+            OddPositionSum += (DoubledDigit- 9);
         }
         else
         {
