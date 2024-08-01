57716209b80b44a48666c3b06d0186996c83f5c7
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-01 04:09:30 +0000

    Wed, Jul 31, 2024, 11:09 PM -05:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,7 +5,7 @@
 
 int Points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int computer_score(string word);
+int compute_score(string word);
 
 int main(void)
 {
