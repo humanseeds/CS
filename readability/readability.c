bd3cbe38daa62546739ef954b5b724a7f13f27d2
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-01 04:56:11 +0000

    Wed, Jul 31, 2024, 11:56 PM -05:00

--- readability/readability.c
+++ readability/readability.c
@@ -21,7 +21,7 @@ int sentences = count_sentences(text);
 
 // Compute Coleman-Liau formula
 // use round function from math.h
-// cast as float so decimals arent truncated. 
+// cast as float so decimals arent truncated.
  index = 0.0588 * L - 0.296 * S - 15.8
 l = (#letters / words) / 100
 s = (#sentences / words0 / 100)
@@ -29,7 +29,7 @@ s = (#sentences / words0 / 100)
 
 
 // Print Grade Level
-
+ printf(")
 
 }
 
