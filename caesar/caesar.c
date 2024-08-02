d41ea331ac763cbfead03f57fbd6972c47f0b1b5
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-02 20:05:37 +0000

    Fri, Aug 2, 2024, 3:05 PM -05:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -9,10 +9,19 @@ int main (int argc, string argv[])
     if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
-        return 1
+        return 1;
     }
 
-    else if a
+    for (int i = 0; i < strlen(argv[1]), i++)
+    {
+       if (!isdigit(argv[1][i]))
+       {
+        printf("Usage: ./caesar key\n");
+        return 1;
+       }
+
+
+    }
 
 }
 
