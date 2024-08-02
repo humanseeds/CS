182ebf1b035e4d966b282302da03b5b62b3f376e
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-02 05:37:00 +0000

    Fri, Aug 2, 2024, 12:37 AM -05:00

--- readability/readability.c
+++ readability/readability.c
@@ -56,7 +56,7 @@ int count_letters(string text)
 int count_words(string text)
 {
     //retrun number of words in text)  (isalpha - isblank) + 1
-    int words = 1
+    int words = 1;
     for (int i = 0; i < strlen(text); i++)
     {
         if (isspace((text[1]))
@@ -70,7 +70,7 @@ int count_words(string text)
 int count_sentences(string text)
 {
     //count number of sentences in text.   ispunct
-    int sentences = 0
+    int sentences = 0;
     for (int i = 0; i < strlen(text); i++)
     if (text[i] = "." || text[i] = "!" || text[i] = "?")
     {
