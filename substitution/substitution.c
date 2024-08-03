c0ba9f7f135da107de33b09fb867252aac498a17
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-03 02:42:31 +0000

    Fri, Aug 2, 2024, 9:42 PM -05:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -9,7 +9,7 @@ int main(int argc, string argv[])
 {
      if (argc != 2)
     {
-      printf("./substiution key" /n);
+      printf("Usage: ./substiution key" /n);
       return = 1;
     }
     for (i =0; 1 < strlen(argv[1]); i++)
