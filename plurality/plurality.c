2bf808c3327691858b83c71d507759c322a0c75b
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-05 00:03:18 +0000

    Sun, Aug 4, 2024, 7:03 PM -05:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -83,6 +83,7 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    // start a vote counter at zero
+    // iterate over each
     return;
 }
