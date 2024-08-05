d8d0135495a1085fce894401f7595bf0d175abdd
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-05 19:58:29 +0000

    Mon, Aug 5, 2024, 2:58 PM -05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -142,6 +142,13 @@ bool vote(int voter, int rank, string name)
 void tabulate(void)
 {
     // TODO
+    for (int i = 0, i < voter_count; i++)
+    {
+        if ()
+        {
+            
+        }
+    }
     return;
 }
 
