ed414af49a3f5bc8251f6ef32d37c18ca9d5d30c
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-06 00:26:36 +0000

    Mon, Aug 5, 2024, 7:26 PM -05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,6 +130,7 @@ bool vote(int voter, int rank, string name)
     // iterate over the candidates
     for (int i = 0; 1 < candidate_count; i++)
     {
+        // compair the candidate to 
         if (strcomp(candidates[i].name, name) == 0)
         {
             preferences[voter][rank] = i;
