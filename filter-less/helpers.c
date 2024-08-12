6eedab0a4886eb047ad3f3af5a5b178089a26e7b
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-12 01:20:55 +0000

    Sun, Aug 11, 2024, 8:20 PM -05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -22,6 +22,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
+// create a helper function to ensure rgbt values dont exceed 255
+int capped255 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -33,7 +35,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int sepiaRed = round((.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) + (.189 * image[i][j].rgbtBlue));
             int sepiaGreen = round((.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + (.168 * image[i][j].rgbtBlue));
             int sepiaBlue = round((.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + (.131 * image[i][j].rgbtBlue));
-        
+
         }
     }
 
