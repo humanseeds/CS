f81a85621e806bcb27d9b637eaefef11bd0b2ad5
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-13 01:13:44 +0000

    Mon, Aug 12, 2024, 8:13 PM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -16,6 +16,9 @@ int main(int argc, char *argv[])
     // Create a buffer for a block of data
     uint8_t buffer[512];
 
+    // create a counter to make sure all 50 files are found
+    int file_counter = 0;
+    
     // While there's still data left to read from the memory card
     while (fread(buffer, 1, 512, card) == 512)
     {
