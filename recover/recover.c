7b42dce733699a14fb68a78063bbd88a69c49f3b
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-13 00:44:32 +0000

    Mon, Aug 12, 2024, 7:44 PM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     // While there's still data left to read from the memory card
     while (fread(buffer, 1, 512, card) == 512)
     {
-        int i =0; i < 
+        int i = 0; i < 
         // Create JPEGs from the data
        for (buffer[0] == 0xff &&
             buffer[1] == 0xd8 &&
