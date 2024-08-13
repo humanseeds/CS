6e25052bbcb495510867cce6034852f2dba52961
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-13 00:40:33 +0000

    Mon, Aug 12, 2024, 7:40 PM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -19,15 +19,16 @@ int main(int argc, char *argv[])
     // While there's still data left to read from the memory card
     while (fread(buffer, 1, 512, card) == 512)
     {
+        int i =0; i < 
         // Create JPEGs from the data
-        if (buffer[0] == 0xff &&
+       for (buffer[0] == 0xff &&
             buffer[1] == 0xd8 &&
             buffer[2] == 0xff &&    // if first jpeg starts with certain jpg identifiers
            (buffer[3] & 0xf0) == 0xe0)
             {
                 sprintf(filename, "%03i.jpg",x ) // create a new file. rmemeber to create an actual filename and  replace x
             }
-                
+
         }
     }
 }
