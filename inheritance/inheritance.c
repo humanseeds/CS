7e68a558e321f2ee79ebd3ca0576039d56e3624d
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-19 21:17:46 +0000

    Mon, Aug 19, 2024, 4:17 PM -05:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -53,15 +53,20 @@ person *create_family(int generations)
         new_person->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        new_person->alleles[0] = parent->alleles[]
+        new_person->alleles[0] = parent->alleles[rand() % 2];
+        new_person->alleles[1] = parent->alleles[rand() % 2];
     }
 
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
+        new_person->parents[0] = NULL;
+        new_person->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
+        new_person->alleles[0] = random_allele();
+        new_person->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
