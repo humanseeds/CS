196aba0aa33371e4a562c50de6cb08f38700a6f0
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-19 01:50:19 +0000

    Sun, Aug 18, 2024, 8:50 PM -05:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,10 +1,10 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 int main(int argc, char *argv[])
 {
-   // Accept a single command-line argument
+    // Accept a single command-line argument
     if (argc != 2)
     {
         printf("Usage: ./recover FILE\n");
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     // Open the memory card
     FILE *card = fopen(argv[1], "r");
 
-     // define a block size to eliminate magic numbers
+    // define a block size to eliminate magic numbers
     int block_size = 512;
 
     // Create a buffer for a block of data
@@ -23,48 +23,46 @@ int main(int argc, char *argv[])
     // create a counter to properly label each new jpg file
     int jpg_count = 0;
 
-    //create a pointer to the currently opened jpg file
+    // create a pointer to the currently opened jpg file
     FILE *img = NULL;
 
     //  loop  while there's still data left to read from the memory card
     while (fread(buffer, 1, block_size, card) == block_size)
     {
         // check if the block is a new jpg
-        if (buffer[0] == 0xff &&
-            buffer[1] == 0xd8 &&
-            buffer[2] == 0xff &&
-           (buffer[3] & 0xf0) == 0xe0)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
+            (buffer[3] & 0xf0) == 0xe0)
         {
-            //if a jpg file is already open, close it
+            // if a jpg file is already open, close it
             if (img != NULL)
             {
                 fclose(img);
             }
 
-    // generate a new file name. create an 8 digit array
-    char recovered[8];
-    sprintf(recovered, "%03i.jpg", jpg_count);
+            // generate a new file name. create an 8 digit array
+            char recovered[8];
+            sprintf(recovered, "%03i.jpg", jpg_count);
 
-    // open the newly created recovered file
-    img = fopen(recovered, "w");
+            // open the newly created recovered file
+            img = fopen(recovered, "w");
 
-    // check for error creating thr file
-    if (img == NULL)
-    {
-        fclose(card);
-        return 1;
+            // check for error creating thr file
+            if (img == NULL)
+            {
+                fclose(card);
+                return 1;
+            }
+
+            // increment jpg counter
+            jpg_count++;
+        }
+
+        // if the jpg is open, write the current block to it
+        if (img != NULL)
+        {
+            fwrite(buffer, 1, block_size, img);
+        }
     }
-
-    //increment jpg counter
-    jpg_count++;
-}
-
-    // if the jpg is open, write the current block to it
-    if (img != NULL)
-    {
-        fwrite(buffer, 1, block_size, img);
-    }
-}
 
     // close any remaining files
     if (img != NULL)
@@ -72,6 +70,6 @@ int main(int argc, char *argv[])
         fclose(img);
     }
 
-fclose(card);
-return 0;
+    fclose(card);
+    return 0;
 }
