a321f914a2718df29f53d902a6653ab23c056cab
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-08-25 00:31:45 +0000

    Sat, Aug 24, 2024, 7:31 PM -05:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -60,7 +60,7 @@ bool load(const char *dictionary)
             return false;
         }
         // copy the word into the node
-      strcpy(new_node->word, buffer);)
+      strcpy(new_node->word, buffer);
 
         //hash word to obtain a hash value
 
