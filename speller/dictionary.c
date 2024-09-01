1153c87ac10755c5abc5ae72ff8cf0db760bb6c5
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-01 23:58:35 +0000

    Sun, Sep 1, 2024, 6:58 PM -05:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -17,6 +17,9 @@ typedef struct node
 // TODO: Choose number of buckets in hash table
 const unsigned int N = 26;
 
+//create a global vriable to track the number of words loaded
+unsigned in (word_count) = 0
+
 // Hash table
 node *table[N];
 
@@ -103,8 +106,8 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+
+    return word_count;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
