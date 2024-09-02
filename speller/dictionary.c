9db59b25787e6024a832f15bfaa5085f03057aca
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-02 05:00:58 +0000

    Mon, Sep 2, 2024, 12:00 AM -05:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -1,12 +1,12 @@
 // Implements a dictionary's functionality
 
+#include "dictionary.h"
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include "dictionary.h"
-#include <strings.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <strings.h>
 
 // Represents a node in a hash table
 typedef struct node
@@ -18,7 +18,7 @@ typedef struct node
 // TODO: Choose number of buckets in hash table
 const unsigned int N = 5381;
 
-//create a global vriable to track the number of words loaded
+// create a global vriable to track the number of words loaded
 unsigned int word_count = 0;
 
 // Hash table
@@ -30,49 +30,50 @@ bool check(const char *word)
     // use the right index in the hash table for the word
     int index = hash(word);
 
-    //create a pointer to traverse the liniked list
+    // create a pointer to traverse the liniked list
     node *cursor = table[index];
 
-    //traverse the linked list as long as the word is not found
+    // traverse the linked list as long as the word is not found
     while (cursor != NULL)
+    {
+
+        // case insensitive comparison of the word in the node vs the searched word
+        if (strcasecmp(cursor->word, word) == 0)
         {
-
-            //case insensitive comparison of the word in the node vs the searched word
-            if (strcasecmp(cursor->word, word) == 0)
-            {
-               //if the word is found, return true
-                return true;
-            }
-
-            //move the cursor to the next node
-            cursor = cursor->next;
+            // if the word is found, return true
+            return true;
         }
+
+        // move the cursor to the next node
+        cursor = cursor->next;
+    }
 
     // if the word is not found return false
     return false;
 }
 
 // Hashes word to a number
-//at 7:35 in the hash tables short video Doug Loyd sayd to not write our own hashfunction
+// at 7:35 in the hash tables short video Doug Loyd sayd to not write our own hashfunction
 // instead find a hash function online and give credit to the athor.
-//the DJB2 hash function is from Daniel J. Bernstein
+// the DJB2 hash function is from Daniel J. Bernstein
 unsigned int hash(const char *word)
 {
-    //initialize the hash to a large prime number
+    // initialize the hash to a large prime number
     unsigned long hash = 5381;
 
-    //create a variable to store each character of the string'word'
+    // create a variable to store each character of the string'word'
     int c;
 
-    //loop through the string, assign the value of each char to c, and increment to the next char in the string
+    // loop through the string, assign the value of each char to c, and increment to the next char
+    // in the string
     while ((c = *word++))
     {
 
-        //convert to lowercase to be case insensitive
+        // convert to lowercase to be case insensitive
         c = tolower(c);
 
-        //hash = hash * 32 + c. the << operator shifts the bits by 5 positions (each bit shift
-        //multiples by 2, a 5 position shift is 2^5 or 32)
+        // hash = hash * 32 + c. the << operator shifts the bits by 5 positions (each bit shift
+        // multiples by 2, a 5 position shift is 2^5 or 32)
         hash = ((hash << 5) + hash) + c;
     }
     // return the value of the hashed value modulated by the number of buckets
@@ -82,24 +83,24 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    //open the dictionary file, print error if unable
+    // open the dictionary file, print error if unable
     FILE *file = fopen(dictionary, "r");
     if (file == NULL)
     {
         printf("Unable to open %s\n", dictionary);
-    return false;
+        return false;
     }
 
     // create a temp variable to hold each word from file
-    char input_word[LENGTH +1];
+    char input_word[LENGTH + 1];
 
-    //read strings from the file
+    // read strings from the file
     while (fscanf(file, "%s", input_word) != EOF)
     {
         // create a new node for each word
         node *new_node = malloc(sizeof(node));
 
-        //if the memory cannot be allocated, close the file, and free memory leaks
+        // if the memory cannot be allocated, close the file, and free memory leaks
         if (new_node == NULL)
         {
             fclose(file);
@@ -107,23 +108,23 @@ bool load(const char *dictionary)
             return false;
         }
         // copy the word into the node
-    strcpy(new_node->word, input_word);
+        strcpy(new_node->word, input_word);
 
-    //hash word to obtain a hash value
-    int index = hash(input_word);
+        // hash word to obtain a hash value
+        int index = hash(input_word);
 
-    // insert the new node into the hash function.
-    new_node->next = table[index];
-    table[index] = new_node;
+        // insert the new node into the hash function.
+        new_node->next = table[index];
+        table[index] = new_node;
 
-     //increment the word count
-    word_count++;
+        // increment the word count
+        word_count++;
     }
 
-    //close the dictionary source file
+    // close the dictionary source file
     fclose(file);
 
-    //return true if the function was successful
+    // return true if the function was successful
     return true;
 }
 
@@ -137,18 +138,17 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    //iterate over each bucket of the hash table
+    // iterate over each bucket of the hash table
     for (int i = 0; i < N; i++)
     {
-        //skip a bucket if it is empty
-          if (table[i] == NULL)
+        // skip a bucket if it is empty
+        if (table[i] == NULL)
         {
             continue;
         }
 
-         //set a pointer to the head of the list
+        // set a pointer to the head of the list
         node *cursor = table[i];
-
 
         // run a loop while the pointer is not at the end of the linked list
         while (cursor != NULL)
@@ -163,6 +163,6 @@ bool unload(void)
             cursor = temp;
         }
     }
-   //return true to indicate the memory is properly free
+    // return true to indicate the memory is properly free
     return true;
 }
