99c6abcadc6c57e5b9b87fffcf7b577e253374c4
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-04 19:24:11 +0000

    Wed, Sep 4, 2024, 2:24 PM -05:00

---
+++ credit.py
@@ -0,0 +1,22 @@
+
+
+
+# Prompt for user input
+
+
+# multiply every other digit
+
+
+# sum those digits
+
+
+# add to remaining digits
+
+
+# validate checksum
+
+
+# validate company's number
+
+
+# validate number's length
