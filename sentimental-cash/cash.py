c4ba835e6d0d8985b260ee9981e53267e436c5c8
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-04 15:52:04 +0000

    Wed, Sep 4, 2024, 10:52 AM -05:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -8,20 +8,3 @@ while True:
 
 change = round(change, 2)
 
-denominations = [
-    "quater": {"value": 0.25, "count" : 0},
-    "dimes": {"value" : 0.10, "count" : 0},
-    "nickel": {"value" : 0.05, "count" : 0},
-    "penny": {"value" : 0.01, "count" : 0},
-]
-
-
-for coin in denominations:
-    value = denominations[coin]["value"]
-    while change >= value:
-        change -= value
-        change =
-        denominations[coin]['count'] += 1
-
-for coin in denominations:
-    print(f"{coin}: {'count'}")
