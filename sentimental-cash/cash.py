13a4a35149144649ffd98175709b0aa29309a2fc
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-04 04:26:20 +0000

    Tue, Sep 3, 2024, 11:26 PM -05:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,13 +1,19 @@
 from cs50 import get_float
 
-while True:
-    change = get_float("Change: ")
-    if change > 0:
-        break
+
 
 denominations = {
-    
-
-
+    'quater': {'value': 0.25, 'count' : 0},
+    'dimes': {'value' : 0.10, 'count' : 0},
+    'nickel': {'value' : 0.05, 'count' : 0},
+    'penny': {'value' : 0.01, 'count' : 0},
 }
 
+
+for coin in denominations:
+    value = denominations[coin]['value']
+    while change > value:
+        change -= value
+        denominations[coin['count'] += 1
+
+for coin in denominations.items
