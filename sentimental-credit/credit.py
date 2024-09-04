fbdc72d5bc46d6816dbfda0894e572b6a65bba8a
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-04 20:05:36 +0000

    Wed, Sep 4, 2024, 3:05 PM -05:00

---
+++ sentimental-credit/credit.py
@@ -0,0 +1,26 @@
+from cs50 import get_float
+
+
+# Prompt for user input
+while True:
+    CC_number = get_float("Please Input Credit Card Numer: ")
+    if CC_number >= 0:
+        break
+
+
+# multiply every other digit
+
+
+# sum those digits
+
+
+# add to remaining digits
+
+
+# validate checksum
+
+
+# validate company's number
+
+
+# validate number's length
