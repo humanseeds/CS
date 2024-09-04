7845e9feb5b71fec539fc9819617d81bcc0bb12d
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-04 16:06:44 +0000

    Wed, Sep 4, 2024, 11:06 AM -05:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,16 +1,27 @@
 from cs50 import get_float
 
-
+# ask user for how much change is owed.
+#loop until amount is greater than zero
 while True:
    change = get_float("Change Owed: ")
    if change  >= 0:
     break
 
+# round the amount of change to 0.00 to avoid floating error
 change = round(change, 2)
 
+#create a list of denominations for quaters, dimes, nickles, pennies
 coins = [25, 10, 5, 1]
+
+#create a coin counter set to zero
 coin_count = 0
 
-while change > 0
-    change - coins[]
-    coin_count+=
+#itterate over the list of denominations
+#increase coin coun after each loop
+for coin in coins:
+    while change >= coin:
+    change -= coin
+    coin_count +=
+
+#print the coin count
+print(coin_count)
