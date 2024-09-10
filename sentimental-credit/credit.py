492fdb1d7208c2aa280c574faffc71b8ec3eb0a7
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-10 18:03:22 +0000

    Tue, Sep 10, 2024, 1:03 PM -05:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,12 +1,12 @@
-from cs50 import get_string
+from cs50 import get_int
 
 
 # Prompt for user input
 while True:
-    CC_number = get_string("Please Input Credit Card Numer: ")
+    CC_number = get_int("Please Input Credit Card Numer: ")
     if CC_number >= 0:
         break
-        
+
 
 # multiply every other digit
 
