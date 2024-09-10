3d065040edcffb1de1072f2faef348ad82fcad3a
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-10 18:43:38 +0000

    Tue, Sep 10, 2024, 1:43 PM -05:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,15 +2,17 @@ from cs50 import get_int
 
 
 # Prompt for user for a credit card number
-
 while True:
     card_number = int(input("please input credit card number: "))
     if card_number > 0:
         break
     print("invalid credit card number, please try again.")
+
+#initialize variables to be used in the program to 0
 working_cnn = get_int
 even_positions = 0
 odd_positions = 0
+
 # multiply every other digit
 
 
