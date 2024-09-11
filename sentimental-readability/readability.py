b2bbc03eefb86d958c8a437c5e5e2b0962e32e39
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-11 04:15:22 +0000

    Tue, Sep 10, 2024, 11:15 PM -05:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -23,7 +23,7 @@ for characters in text:
 
 # computer the Coleman-Liau index
 L = (num_letters / num_words) * 100
-S = (Num_sentences / Num_words) * 100
+S = (num_sentences / num_words) * 100
 index = 0.0588 * L - 0.296 * S - 15.8
 
 # Print the grade level
