2619ca5c246ff7a994c3fbb18d9814e9f3dc1914
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-11 00:58:55 +0000

    Tue, Sep 10, 2024, 7:58 PM -05:00

---
+++ sentimental-readability/readability.py
@@ -0,0 +1,18 @@
+from cs50 import get_string
+
+# promt user for text
+text = get_string("Input Text: ")
+
+# count the number of letters in the text
+
+
+# count the number of words in the text
+
+
+# count the number of sentences in the text
+
+
+# computer the Coleman-Liau index
+
+
+# Print the grade level
