4397239e468ccea3755bd0c296766695c788dca7
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-11 04:11:31 +0000

    Tue, Sep 10, 2024, 11:11 PM -05:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -18,7 +18,7 @@ for character in text:
 # count the number of sentences in the text
 num_sentences = 0
 for characters in text:
-    if character in [".", "!", "?"]
+    if character in [".", "!", "?"]:
         num_sentences += 1
 
 # computer the Coleman-Liau index
