1ec8b12a8ba60687d99e7dcc70fe845b446e45b4
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-11 00:35:01 +0000

    Tue, Sep 10, 2024, 7:35 PM -05:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -8,7 +8,7 @@ while True:
         break
     print("invalid credit card number, please try again.")
 
-#initialize variables to be used in the program to 0
+# initialize variables to be used in the program to 0
 working_card = card_number
 even_position_sum = 0
 odd_position_sum = 0
@@ -16,53 +16,49 @@ position = 0
 
 # determine the checksum
 while working_card > 0:
-    digit = working_card % 10 # % 10 removes last digit
+    digit = working_card % 10  # % 10 removes last digit
     working_card //= 10  # //10 to move to next digit
 
    # %2 determines if digit is even (0) or odd (1)
     if position % 2 == 0:
-      even_position_sum += digit  #even digits are summed
+        even_position_sum += digit  # even digits are summed
 
-   #odd digits are doubled and then summed
+   # odd digits are doubled and then summed
     else:
-        doubled_digit = digit *2
+        doubled_digit = digit * 2
         if doubled_digit > 9:
-            #subtract 9 to find the sum of the 2 digits b
+            # subtract 9 to find the sum of the 2 digits b
             odd_position_sum += doubled_digit - 9
-        else :
+        else:
             odd_position_sum += doubled_digit
     position += 1
 
-#find the checksum by adding the product of the even and odd positions
+# find the checksum by adding the product of the even and odd positions
 checksum = odd_position_sum + even_position_sum
 
 # if the checksum doesnt end in 0 the card is invalid
 if checksum % 10 != 0:
-   print("Invalid")
+    print("INVALID")
 
 # determine the card length of the valid card number
 else:
     length = 0
     temporary = card_number
     while temporary > 0:
-         temporary //= 10 # again %10 removes each digit
-         length += 1   #counter increments
+        temporary //= 10  # again %10 removes each digit
+        length += 1  # counter increments
 
-    #determine the first 2 digits of the credit card
+    # determine the first 2 digits of the credit card
     starting_digits = card_number
     while starting_digits >= 100:
-         starting_digits //= 10
+        starting_digits //= 10
 
-   # sort out which company the credit card number belongs to
+    # sort out which company the credit card number belongs to
     if length == 15 and (starting_digits == 34 or starting_digits == 37):
-         print("AMEX")
+        print("AMEX")
     elif length == 16 and (starting_digits >= 51 and starting_digits <= 55):
-         print("MASTERCARD")
+        print("MASTERCARD")
     elif (length == 13 or length == 16) and (starting_digits // 10 == 4):
-         print("VISA")
+        print("VISA")
     else:
-         print("INVALID")
-
-
-
-
+        print("INVALID")
