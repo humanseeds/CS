3d800b10240a415678d8e7740d2a47e80004b878
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-28 23:59:55 +0000

    Sat, Sep 28, 2024, 6:59 PM -05:00

--- songs/7.sql
+++ songs/7.sql
@@ -1,3 +1,1 @@
-SELECT AVG(energy) FROM songs
-JOIN artists ON songs.artists_id = artists.id
-WHERE artists.name - 'Drake';
+SELECT AVG(energy) FROM songs JOIN artists ON songs.artists_id = artists.id WHERE artists.name - 'Drake';
