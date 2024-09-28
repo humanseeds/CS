0141a5d731c0b669a17c6683b53ce96daab2c1b6
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-28 23:58:54 +0000

    Sat, Sep 28, 2024, 6:58 PM -05:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,4 +1,1 @@
-SELECT names FROM songs WHERE artist_id = Select id
-    FROM artist
-    WHERE name = 'Post Malone'
-);
+SELECT names FROM songs WHERE artist_id = (Select id FROM artist WHERE name = 'Post Malone');
