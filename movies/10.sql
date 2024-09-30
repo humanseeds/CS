821722e62483034e9ae9b856814b9fdc0a207407
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-30 02:39:53 +0000

    Sun, Sep 29, 2024, 9:39 PM -05:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,1 +1,6 @@
-SELECT 
+SELECT DISTINCT people.name
+FROM people
+JOIN directors on people.id = directors.person_id
+JOIN movies on directors.movie_id = movies.id
+JOIN ratings on movies.id = ratings.movie_id
+WHERE rating >= 9.0;
