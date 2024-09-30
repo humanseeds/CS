88e2e8fd89a0401a36fb7965150901f0508252c6
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-30 04:17:44 +0000

    Sun, Sep 29, 2024, 11:17 PM -05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,10 +3,11 @@
 -- All you know is that the theft took place on July 28, 2023
 -- and that it took place on Humphrey Street.
 
-
+-- Find crime scene description
 SELECT description
 FROM crime_scene_reports
 WHERE month = 7 AND day = 28
 AND street = 'Humphrey Street';
-
+-- 3 witnesses mentioning bakery,
+--littering took place at 4.36pm
 
