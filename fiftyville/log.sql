b2e8f3094ed4733c672a918309977dece036480d
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-30 15:33:26 +0000

    Mon, Sep 30, 2024, 10:33 AM -05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -10,10 +10,10 @@ FROM crime_scene_reports
 WHERE month = 7 AND day = 28
 AND street = 'Humphrey Street';
 -- 3 witnesses mentioning bakery,
--- and additional littering crime took place at 4:36pm no witnesses
+-- and additional littering crime took place at 16:36 no witnesses
 -- from this I need to search the interviews and bakery_security_logs
 
 SELECT activity
-FROM bakery_secuirty_logs
-WHERE 
+FROM bakery_security_logs
+WHERE month = 7 AND day = 28 AND hour = 16 AND minute = 36;
 
