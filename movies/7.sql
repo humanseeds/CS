b2fb49b563fd8792e0868b1fb3d4da7c1440619b
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-30 01:54:53 +0000

    Sun, Sep 29, 2024, 8:54 PM -05:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,3 +1,5 @@
 SELECT movies.titles, ratings.rating
 FROM movies
-JOIN ratings ON
+JOIN ratings ON movies.id = ratings.movie_id
+WHERE movie.year = 2010
+ORDER BY ratings.rating DESC, movies.title ASC;
