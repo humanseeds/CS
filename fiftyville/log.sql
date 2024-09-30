ed5076db489af38e30a1e92cd597bae6371985ef
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-30 04:11:14 +0000

    Sun, Sep 29, 2024, 11:11 PM -05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- All you know is that the theft took place on July 28, 2023
+-- and that it took place on Humphrey Street.
