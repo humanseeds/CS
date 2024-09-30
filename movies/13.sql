76cd8a8734ff076ddbbe530a2952379f9de48bb0
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-30 03:51:02 +0000

    Sun, Sep 29, 2024, 10:51 PM -05:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,5 +1,5 @@
 SELECT people.names
 FROM people
 JOIN stars ON people.id = stars.person_id
-JOIN movie ON stars.movie_id = movies.id
-WHERE movies.id
+JOIN movies ON stars.movie_id = movies.id
+JOIN 
