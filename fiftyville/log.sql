5a865f478f4d859d2c2141727b3aedf0743c8d9e
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-30 16:46:26 +0000

    Mon, Sep 30, 2024, 11:46 AM -05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -10,7 +10,7 @@ FROM crime_scene_reports
 WHERE month = 7 AND day = 28
 AND street = 'Humphrey Street';
 -- 3 witnesses mentioning bakery,
--- and additional littering crime took place at 16:36 no witnesses
+-- and additional littering crime took place at 10:15 no witnesses
 -- from this I need to search the interviews and bakery_security_logs
 
 SELECT transcript
