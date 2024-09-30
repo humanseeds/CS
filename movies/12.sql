04359c126231fb2597c18aab8fa0b17a984d70c9
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-30 03:12:46 +0000

    Sun, Sep 29, 2024, 10:12 PM -05:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,5 +1,5 @@
-SELECT tiles
+SELECT title
 FROM movies
 JOIN stars on movies.id = stars.movie_id
 JOIN people on stars.person_id = people.id
-WHERE people.name IN ('Bradley Cooper','Jennifer Lawrence')
+WHERE people.name IN ('Bradley Cooper','Jennifer Lawrence');
