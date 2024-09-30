66457d7c2f27cba7b3a8a59546d70c2879c0d982
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-30 17:56:49 +0000

    Mon, Sep 30, 2024, 12:56 PM -05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -48,5 +48,5 @@ SELECT atm_transactions.account_number, people.name
 FROM atm_transactions
 JOIN bank_accounts ON atm_transactions.account_number = bank_accounts.account_number
 JOIN people ON bank_accounts.person_id = people.id
-WHERE atm_location = 'Leggett Street'
+WHERE atm_transactions.atm_location = 'Leggett Street'
 AND year = 2023 AND month = 7 and day = 28 AND atm_transactions.transaction_type = 'withdrawal';
