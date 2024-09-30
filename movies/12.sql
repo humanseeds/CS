7df735af0fc6e89edbd1d487821528d8914a8d1f
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-09-30 03:14:53 +0000

    Sun, Sep 29, 2024, 10:14 PM -05:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,4 +2,5 @@ SELECT title
 FROM movies
 JOIN stars on movies.id = stars.movie_id
 JOIN people on stars.person_id = people.id
-WHERE people.name IN ('Bradley Cooper','Jennifer Lawrence');
+WHERE people.name IN ('Bradley Cooper','Jennifer Lawrence')
+GROUP BY movies.title;
