e1bb366f79ef203720b3585b470dc526e1041d60
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-10-29 18:46:25 +0000

    Tue, Oct 29, 2024, 1:46 PM -05:00

--- homepage/functions.js
+++ homepage/functions.js
@@ -12,26 +12,3 @@ function showPage() {
 */
 
 
-function myFunction() {
-  const images = document.querySelectorAll("img"); // Select all images
-  let loadedImages = 0;
-
-  images.forEach((img) => {
-      // Add an event listener for each image
-      img.addEventListener("load", imageLoaded);
-      img.addEventListener("error", imageLoaded); // Handle errors as well
-  });
-
-  function imageLoaded() {
-      loadedImages++;
-      // Check if all images are loaded
-      if (loadedImages === images.length) {
-          showPage();
-      }
-  }
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
