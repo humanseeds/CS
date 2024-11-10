342ad96c83bfa1b6304a29fe1f570ac503c57f20
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-11-10 23:43:58 +0000

    Sun, Nov 10, 2024, 5:43 PM -06:00

--- finance/app.py
+++ finance/app.py
@@ -156,9 +156,6 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    # when requested via GET, display registration form
-     # create a new template for registraion from templates folder
-        # borrow from login.html
     if request.method == "GET":
         return render_template("register.html")
 
