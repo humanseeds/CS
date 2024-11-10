e499254c89a76df05f2984825572f716d8764e8d
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-11-10 18:23:07 +0000

    Sun, Nov 10, 2024, 12:23 PM -06:00

--- finance/app.py
+++ finance/app.py
@@ -161,9 +161,12 @@ def register():
         # borrow from login.html
     if request.method == "GET":
        return render_template("register.html")
-    else: request.method == "POST":
-        
 
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        
         # create a new template for registraion from templates folder
         # borrow from login.html
         # HTML <input name= "password".../>
