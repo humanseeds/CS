300eff360ee5422e5ff5c08e297f16d0ab4af931
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-11-11 00:39:18 +0000

    Sun, Nov 10, 2024, 6:39 PM -06:00

--- finance/app.py
+++ finance/app.py
@@ -176,7 +176,7 @@ def register():
         hash = generate_password_hash(password)
 
         try:
-            db.execute("INSERT INTO users (username, hash) VALUES(?,?)", username, hash)
+            new_user = db.execute("INSERT INTO users (username, hash) VALUES(?,?)", username, hash)
         except:
             return apology("Username Already Exists")
 
