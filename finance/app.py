e4b2697dac1155f8b6606d0fe0ecfdcb63f694c8
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-11-23 23:55:08 +0000

    Sat, Nov 23, 2024, 5:55 PM -06:00

--- finance/app.py
+++ finance/app.py
@@ -37,28 +37,6 @@ def index():
     """Show portfolio of stocks"""
 
 
-    stocks = db.execute("""SELECT symbol, SUM(shares) AS total_shares FROM transactions
-                        WHERE user_id = :user_id GROUP BY symbol HAVING total_shares > 0""",
-                        user_id=session["user_id"])
-
-
-    cash = db.execute("SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["cash"]
-
-    total_value =cash
-
-    for stock in stocks:
-        quote = lookup(stock["symbol"])
-        stock["name"] = quote["name"]
-        stock["price"] = quote["price"]
-        stock["value"] = stock["price"] * stock["total_shares"]
-        total_value += stock["value"]
-
-    return render_template("index.html", stocks=stocks, cash=cash, total_value=total_value)
-
-
-
-    return apology("TODO")
-
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
