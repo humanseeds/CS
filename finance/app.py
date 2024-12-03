94634ffd4fd229dac65a1afe497cceceeb798d42
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-12-03 01:55:59 +0000

    Mon, Dec 2, 2024, 7:55 PM -06:00

--- finance/app.py
+++ finance/app.py
@@ -324,7 +324,8 @@ def sell():
             return apology("Not enough shares for sell order")
 
         # get the current stock prive
-        
+        stock_price = lookupup(symbol)
+        sale_value = stock_price["price"] * shares
 
 
     return apology("TODO")
