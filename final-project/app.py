0f5842461ebb71b88b3a5bf25051d88fa9182a61
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-12-12 18:46:20 +0000

    Thu, Dec 12, 2024, 12:46 PM -06:00

--- final-project/app.py
+++ final-project/app.py
@@ -2,3 +2,6 @@ from flask import Flask, request, render_template, redirect, url_for, send_file
 from werkzeug.utils import secure_filename
 from PIL import Image
 import os
+
+# configure app
+app= Flask(__name__)
