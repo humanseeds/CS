54315252e5f7d93ac7581fd040986a3de7046147
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-12-21 05:30:48 +0000

    Fri, Dec 20, 2024, 11:30 PM -06:00

--- final-project/helpers/helper.py
+++ final-project/helpers/helper.py
@@ -3,13 +3,13 @@ import os
 from PIL import Image, ImageFilter
 
 
-def convert_to_grayscale(image_path)
+def convert_to_grayscale(image_path):
     image = Image.open(image_path)
     grayscale_image = image.convert("L")
     return grayscale_image
 
 
-def apply sobel(image_path)
+def apply_sobel(image_path):
     image = Image.open(image_path)
     grayscale_image = image.convert("L")
     sobel_image = grayscale_image.filter(ImageFilter.Find_EDGES)
