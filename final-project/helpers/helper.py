6c5736f6937fc40747308c6f0a993ec86f6c336a
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-12-22 22:08:39 +0000

    Sun, Dec 22, 2024, 4:08 PM -06:00

--- final-project/helpers/helper.py
+++ final-project/helpers/helper.py
@@ -38,4 +38,8 @@ def Gray_to_ascii(grayscale_image, ascii_chars= " .:;=+*%&@"):
 
     ascci_art = []
     for y in range(grayscale_image.height):
-        row
+        row = ""
+        for x in range(grayscale_image.wdith):
+            brightness + grayscale_image.pixel((x,y))
+            row+= ascii_chards[brightness // scale]
+        ascii_art.append(row)
