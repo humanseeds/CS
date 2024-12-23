7f43dc3682f3d845f1b7e8c5d2fbec0e5702ac30
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-12-23 05:29:48 +0000

    Sun, Dec 22, 2024, 11:29 PM -06:00

--- final-project/app.py
+++ final-project/app.py
@@ -78,7 +78,7 @@ def show_results(filename):
     else:
          ascii_art = None
 
-    return render_template('results.html', original_image=filename,ascii_art=ascii_art)
+    return render_template('results.html', original_image_path=original_image_path,ascii_art=ascii_art)
 
 
 
@@ -93,4 +93,4 @@ def filter_image(filename):
 
 
     # return ascii art or redirect to the results page
-    return redirect(url_for('show_results', filename=filename, ascii_file=ascii_file_path))
+    return redirect(url_for('show_results', filename=filename, ascii_art=ascii_art))
