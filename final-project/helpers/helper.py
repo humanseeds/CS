eece4717d5ef714be6a95e714d1bd7217840ce41
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-12-23 17:04:08 +0000

    Mon, Dec 23, 2024, 11:04 AM -06:00

--- final-project/helpers/helper.py
+++ final-project/helpers/helper.py
@@ -90,7 +90,7 @@ def save_ascii(ascii_art, original_image_path, upload_folder):
     ascii_filename = f"ascii_{os.path.basename(original_image_path)}.txt"
 
     # create a path to the file
-    ascii_file_path = os.path.joiin(upload_folder, ascii_filename)
+    ascii_file_path = os.path.join(upload_folder, ascii_filename)
 
     # open the file with write mode and save the ascii art
     with open(ascii_file_path, "W") as file:
@@ -103,9 +103,9 @@ def save_ascii(ascii_art, original_image_path, upload_folder):
 
 
 # this function pulls all of the prious functions together to create the asci art
-def process_image(image_path, upload_folder)
+def process_image(image_path, upload_folder, new_width)
     # create a working copy of the original image
-    copied_image_path = copy(image_path, upload_folder)
+    copied_image_path = copy_image(image_path, upload_folder)
 
     # open the copied image
     copied_image = Image.open(copied_image_path)
