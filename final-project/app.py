75447883835d72653414c62b463bef7eee7632ce
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-12-23 15:39:44 +0000

    Mon, Dec 23, 2024, 9:39 AM -06:00

--- final-project/app.py
+++ final-project/app.py
@@ -68,7 +68,7 @@ def uploaded_image(filename):
 @app.route('/image/<filename>')
 def show_results(filename):
     # get the path to the uploaded image
-    original_image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    original_image = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
     ascii_file_path = request.args.get('ascii_file')
 
@@ -78,15 +78,13 @@ def show_results(filename):
     else:
          ascii_art = None
 
-    return render_template('results.html', original_image=filename, ascii_art=ascii_art)
+    return render_template('results.html', original_image=original_image, ascii_art=ascii_art)
 
 
 
 
 @app.route('/apply_filter/<filename>')
 def filter_image(filename):
-    # path to the original image
-    original_image = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
     # apply the ascii filter
     ascii_art, ascii_file = apply_filter(original_image, app.config['UPLOAD_FOLDER'])
