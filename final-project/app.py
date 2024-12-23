1110083b183ba3d46d7bad3ab95cad663b73f94e
humanseeds <77948165+humanseeds@users.noreply.github.com>
2024-12-23 15:37:32 +0000

    Mon, Dec 23, 2024, 9:37 AM -06:00

--- final-project/app.py
+++ final-project/app.py
@@ -68,7 +68,7 @@ def uploaded_image(filename):
 @app.route('/image/<filename>')
 def show_results(filename):
     # get the path to the uploaded image
-    original_image = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    original_image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
     ascii_file_path = request.args.get('ascii_file')
 
@@ -78,9 +78,7 @@ def show_results(filename):
     else:
          ascii_art = None
 
-    return render_template('results.html', original_image=filename
-
-                           , ascii_art=ascii_art)
+    return render_template('results.html', original_image=filename, ascii_art=ascii_art)
 
 
 
